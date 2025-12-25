132ffb71472da77696185ff63d5b2437e4890050
gagan2389 <gupta.gagan2906@gmail.com>
2025-12-25 15:29:59 +0530

    feat: make navbar logo a clickable link to the homepage

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import dummydata from '../dummydata.json';
 import logo from '/logo/logo.svg';
 
 const NavBar: React.FC = () => {
   return (
     <nav className="navbar navbar-light bg-light justify-content-between px-3 !fixed !top-0 !w-full !z-50 !shadow-sm">
-      <img src={logo} alt="BookMaro Logo" className="d-inline-block align-top me-2" />
+      <Link to="/">
+        <img src={logo} alt="BookMaro Logo" className="d-inline-block align-top me-2" />
+      </Link>
       <div className="flex flex-row gap-2">
         <i className="bi bi-geo-alt-fill text-indigo-900"></i>
         <p className="h-full !m-0 text-indigo-900">{dummydata?.hero?.location}</p>
